0a4bf23b40ee0ebd208e8fa0176413aad94e9532
Miguel Romeral <miguel.romeral@gmail.com>
2019-06-15 22:17:49 +0200

    Driver details implemented as well. Trying to show with Flickr API images searching drivers names but they are shorted by uploaded date, not relevance. Button and image hiden now.

---
+++ F1_mvc/Controllers/DriversController.cs
@@ -0,0 +1,36 @@
+﻿using F1_mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace F1_mvc.Controllers
+{
+    public class DriversController : Controller
+    {
+        internal static ModelF1 db = new ModelF1();
+
+        // GET: Drivers
+        public ActionResult Index()
+        {
+            return View(db.drivers.ToList());
+        }
+
+
+        public ActionResult Details(string id)
+        {
+            if (id == null || id == "")
+                throw new HttpException(404, "The driver "+id+" requested is not in the database.");
+
+            drivers r = db.drivers.Where(x => x.driverRef == id).FirstOrDefault();
+            if (r == null)
+                throw new HttpException(404, "The driver "+id+" requested is not in the database.");
+
+            ViewBag.Title = r.forename + " " + r.surname;
+
+            return View(r);
+        }
+
+    }
+}
--- F1_mvc/Models/drivers.cs
+++ F1_mvc/Models/drivers.cs
@@ -10,33 +10,42 @@ namespace F1_mvc.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "ID")]
         public int driverId { get; set; }
 
         [Required]
         [StringLength(255)]
+        [Display(Name = "Reference")]
         public string driverRef { get; set; }
 
+        [Display(Name = "Number")]
         public int? number { get; set; }
 
         [StringLength(3)]
+        [Display(Name = "Short name")]
         public string code { get; set; }
 
         [Required]
         [StringLength(255)]
+        [Display(Name = "Forename")]
         public string forename { get; set; }
 
         [Required]
         [StringLength(255)]
+        [Display(Name = "Surname")]
         public string surname { get; set; }
 
         [Column(TypeName = "date")]
+        [Display(Name = "Birth date")]
         public DateTime? dob { get; set; }
 
         [StringLength(255)]
+        [Display(Name = "Nationality")]
         public string nationality { get; set; }
 
         [Required]
         [StringLength(255)]
+        [Display(Name = "Url")]
         public string url { get; set; }
     }
 }
