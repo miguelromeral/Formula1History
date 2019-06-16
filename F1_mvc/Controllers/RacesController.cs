0525877d7b13fc59a9e6c36525e3c0a6b62035e6
Miguel Romeral <miguel.romeral@gmail.com>
2019-06-16 17:23:43 +0200

    Link in race details. A fool.

--- F1_mvc/Controllers/RacesController.cs
+++ F1_mvc/Controllers/RacesController.cs
@@ -14,9 +14,72 @@ namespace F1_mvc.Controllers
         // GET: Races
         public ActionResult Index()
         {
-            var l = db.races.OrderBy(x => x.date).ToList();
-
-            return View(l);
+            return View(db.races.OrderBy(x => x.date).ToList());
         }
+
+
+
+        public ActionResult Details(int id)
+        {
+            var r = GetRaceByID(id);
+            if (r == null)
+                throw new HttpException(404, "The race " + id + " requested is not in the database.");
+            
+            ViewBag.Title = r.year +" "+ r.name;
+
+            return View(r);
+        }
+
+
+        public PartialViewResult RaceDetails(int id)
+        {
+            var r = GetRaceByID(id);
+            if (r == null)
+                throw new HttpException(404, "The race " + id + " requested is not in the database.");
+
+            Dictionary<string, object> stats = new Dictionary<string, object>();
+
+
+            //var query = from res in db.results
+            //            join dri in db.drivers on res.driverId equals dri.driverId
+            //            select dri;
+
+
+            var q = from res in db.results
+                    join dri in db.drivers
+                    on res.driverId equals dri.driverId
+                    where res.raceId == id
+                    select dri;
+
+
+            var d = q.FirstOrDefault();
+            if (d != null)
+            {
+                stats.Add("Winner", d);
+            }
+
+
+
+            return PartialView("_RaceDetails", stats);
+        }
+
+
+
+
+
+
+
+        private races GetRaceByID(int? id)
+        {
+            if (id == null)
+                return null;
+
+            races d = db.races.Where(x => x.raceId == id).FirstOrDefault();
+            if (d == null)
+                return null;
+
+            return d;
+        }
+
     }
 }
