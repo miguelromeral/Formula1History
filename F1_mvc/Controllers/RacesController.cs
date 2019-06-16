c93fd10cb18842982bdf7a51d2d8b8b878cb06a1
Miguel Romeral <miguel.romeral@gmail.com>
2019-06-16 13:52:11 +0200

    Races Index developed (like any other so far).

---
+++ F1_mvc/Controllers/RacesController.cs
@@ -0,0 +1,22 @@
+﻿using F1_mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace F1_mvc.Controllers
+{
+    public class RacesController : Controller
+    {
+        internal static ModelF1 db = new ModelF1();
+
+        // GET: Races
+        public ActionResult Index()
+        {
+            var l = db.races.OrderBy(x => x.date).ToList();
+
+            return View(l);
+        }
+    }
+}
--- F1_mvc/Models/races.cs
+++ F1_mvc/Models/races.cs
@@ -9,25 +9,33 @@ namespace F1_mvc.Models
     public partial class races
     {
         [Key]
+        [Display(Name = "#")]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int raceId { get; set; }
 
+        [Display(Name = "Year")]
         public int year { get; set; }
 
+        [Display(Name = "Round")]
         public int round { get; set; }
 
+        [Display(Name = "Circuit ID")]
         public int circuitId { get; set; }
 
         [Required]
         [StringLength(255)]
+        [Display(Name = "Grand Prix")]
         public string name { get; set; }
 
         [Column(TypeName = "date")]
+        [Display(Name = "Date")]
         public DateTime date { get; set; }
 
+        [Display(Name = "Time")]
         public TimeSpan? time { get; set; }
 
         [StringLength(255)]
+        [Display(Name = "Url")]
         public string url { get; set; }
     }
 }
